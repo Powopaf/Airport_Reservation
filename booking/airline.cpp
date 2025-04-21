770649923d495f02f5072a171b56f12f54654c13
pierre-antoine.fournier <paf.fournier@gmail.com>
2025-04-21 18:53:11 +0800

    add ailiine class

---
+++ booking/airline.cpp
@@ -0,0 +1,26 @@
+#include "airline.h"
+#include <string>
+
+Airline::Airline(std::string name, int id, int nb_passengers, int limit_passengers) 
+    : name(name), id(id), nb_passengers(nb_passengers), limit_passengers(limit_passengers)
+{}
+
+std::string Airline::get_name() const {
+    return name;
+}
+
+int Airline::get_id() const {
+    return id;
+}
+
+int Airline::get_nb_passengers() const {
+    return nb_passengers;
+}
+
+int Airline::get_limit_passengers() const {
+    return limit_passengers;
+}
+
+void Airline::set_nb_paasengers(int passenger) {
+    nb_passengers = passenger;
+}
---
+++ booking/airline.h
@@ -0,0 +1,22 @@
+#ifndef AIRLINE_H
+#define AIRLINE_H
+
+#include <string>
+
+class Airline {
+    private:
+        std::string name;
+        int id;
+        int nb_passengers;
+        int limit_passengers;
+    public:
+        Airline(std::string name, int id, int nb_passengers, int limit_passengers);
+        std::string get_name() const;
+        int get_id() const;
+        int get_nb_passengers() const;
+        int get_limit_passengers() const;
+        void set_nb_paasengers(int passenger=1);
+
+};
+
+#endif
